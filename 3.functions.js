e4e6e39e04e4fd934a4cd092d9754127a7c2800b
[vennapusacharantejareddy] <[charanteja.fullstack@gmail.com]>
2025-09-28 10:25:30 +0530

    javascript

---
+++ 3.functions.js
@@ -0,0 +1,29 @@
+function fisrt(){
+    console.log("good moorning");
+
+}
+fisrt();
+
+    let studentlist=[{
+        "name":"bhanu",
+        "age":23,
+        "college":"aits",
+        "location":"rajampet"
+    },{
+        "name":"prakash",
+        "age":24,
+        "college":"jntua",
+    }];
+    console.log(studentlist);
+
+    function fun1(){
+        console.log("hello bhanu");
+    }
+    fun1();
+    
+
+    function fun2(){
+        console.log(123456); 
+         
+    }
+    fun2(); 
