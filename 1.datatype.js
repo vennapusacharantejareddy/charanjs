a937df996ee9cca261c2e3208cf66cdbff1fc476
[vennapusacharantejareddy] <[charanteja.fullstack@gmail.com]>
2025-09-28 10:06:53 +0530

    javascript

---
+++ 1.datatype.js
@@ -0,0 +1,15 @@
+let name = "John";
+let age = 30;
+let isStudent = false;
+let hobbies = ["reading", "traveling", "gaming"];
+let address = {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345"
+};      
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Hobbies:", hobbies);
+console.log("Address:", address);   
